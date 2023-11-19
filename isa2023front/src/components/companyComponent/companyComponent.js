b7781b47af2eabfce1eb4e5da2395429bd2f4fbb
Nevena Gligorov <114764987+Nevenaa01@users.noreply.github.com>
2023-11-19 02:26:21 +0100

    feat: updating lists of administrators and free dates for company and added view for users

--- isa2023front/src/components/companyComponent/companyComponent.js
+++ isa2023front/src/components/companyComponent/companyComponent.js
@@ -19,6 +19,11 @@ import MenuItem from '@mui/material/MenuItem';
 import Checkbox from '@mui/material/Checkbox';
 import ListItemText from '@mui/material/ListItemText';
 import Chip from '@mui/material/Chip';
+import dayjs from 'dayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -45,27 +50,40 @@ function CompanyComponent() {
     });
 
     const [editMode, setEditMode] = useState(false);
-    const [role, setRole] = useState("admin");
-    const [selectedAdminId, setSelectedAdminId] = useState();
+    const [role, setRole] = useState("companyAdmin");
     const [companyAdministrators, setCompanyAdministrators] = useState([]);
     const [selectedAdmins, setSelectedAdmins] = useState([]);
+    const [selectedFreeDates, setSelectedFreeDates] = useState([]);
 
     useEffect(() => {
         GetCompanyById(id)
             .then((res) => {
-                console.log(res);
                 setCompanyData(res.data);
-                setRole("admin");
+                setSelectedAdmins(res.data.administratorsId);
+                setSelectedFreeDates(res.data.equipemntsFreeMilliseconds)
+                setRole("user");
             })
             .catch((error) => console.error('Error fetching company data:', error));
 
         GetCompanyAdministrators()
             .then((res) => {
                 setCompanyAdministrators(res.data);
-                setSelectedAdminId(res.data[0].id)
             })
             .catch((error) => console.error('Error fetching company administrators data:', error));
     }, [id]);
+
+    function formatDate(date) {
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+
+        return `${day}/${month}/${year}`;
+    }
+
+    const formatMillisecondsToDate = (milliseconds) => {
+        const date = new Date(milliseconds);
+        return formatDate(date);
+    };
 
     const handleEditClick = () => {
         setEditMode(true);
@@ -97,14 +115,26 @@ function CompanyComponent() {
         const selectedAdmins = event.target.value;
         setSelectedAdmins(selectedAdmins);
         companyData.administratorsId = selectedAdmins;
-        console.log(companyData.administratorsId)
     };
 
+    const handleFreeDatesChange = (event) => {
+        const selectedFreeDates = event.target.value;
+        setSelectedFreeDates(selectedFreeDates);
+        companyData.equipemntsFreeMilliseconds = selectedFreeDates;
+    };
+
+    const handleDatePickerChange = (date) => {
+        var convertedDate = new Date(date)
+
+        selectedFreeDates.push(convertedDate.getTime());
+    };
+
+
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10vh'}}>
             <Stack spacing={2} direction="column">
-                <div>
-                    <TableContainer component={Paper} sx={{ maxWidth: 600, margin: 'auto' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '90vw' }}>
+                    <TableContainer component={Paper} sx={{ maxWidth: '90vw', margin: 'auto' }}>
                         <Table>
                             <TableHead>
                                 <TableRow>
@@ -187,24 +217,106 @@ function CompanyComponent() {
                         </FormControl>
 
                     ) : (<div></div>)}
+
+                    {editMode ? (
+                        <FormControl fullWidth sx={{ mt: 2 }}>
+                            <Stack spacing={2} direction="row">
+                                <InputLabel id="demo-multiple-checkbox-label">Free Dates</InputLabel>
+                                <Select
+                                    labelId="demo-multiple-checkbox-label"
+                                    id="demo-multiple-checkbox"
+                                    label="Free Dates"
+                                    multiple
+                                    value={selectedFreeDates}
+                                    onChange={handleFreeDatesChange}
+                                    renderValue={(selected) => (
+                                        <div>
+                                            {selected.map((date) => (
+                                                <Chip key={date} label={formatMillisecondsToDate(date)} />
+                                            ))}
+                                        </div>
+                                    )}
+                                    MenuProps={MenuProps}>
+                                    {companyData.equipemntsFreeMilliseconds.map((date) => (
+                                        <MenuItem key={date} value={date}>
+                                            <Checkbox checked={selectedFreeDates.includes(date)} />
+                                            <ListItemText primary={formatMillisecondsToDate(date)} />
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DemoContainer components={['DatePicker', 'DatePicker']}>
+                                        <DatePicker
+                                            label="Controlled picker"
+                                            onChange={handleDatePickerChange}
+                                        />
+                                    </DemoContainer>
+                                </LocalizationProvider>
+                            </Stack>
+                        </FormControl>
+                    ) : <div></div>}
                 </div>
-                {editMode ? (
-                    <Stack spacing={2} direction="row">
-                        <Button variant="contained" onClick={handleSaveClick}>
-                            Save
-                        </Button>
-                        <Button variant="outlined" onClick={handleCancelClick}>
-                            Cancel
-                        </Button>
-                    </Stack>
+                {role === "companyAdmin" ? (
+                    <div>
+                        {editMode ? (
+                            <Stack spacing={2} direction="row">
+                                <Button variant="contained" onClick={handleSaveClick}>
+                                    Save
+                                </Button>
+                                <Button variant="outlined" onClick={handleCancelClick}>
+                                    Cancel
+                                </Button>
+                            </Stack>
+                        ) : (
+                            <Button variant="contained" onClick={handleEditClick}>
+                                Edit
+                            </Button>
+
+                        )}
+                    </div>
                 ) : (
-                    <Button variant="contained" onClick={handleEditClick}>
-                        Edit
-                    </Button>
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '90vw' }}>
+                        <TableContainer component={Paper} sx={{ maxWidth: '90vw', margin: 'auto', flex: 1 }}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Equipment type</TableCell>
+                                        <TableCell>Grade</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow>
+                                        {editMode ? (
+                                            <>
+                                                <TableCell>
+                                                    <TextField
+                                                        value={companyData.name}
+                                                        onChange={(e) => handleInputChange('name', e.target.value)}
+                                                    />
+                                                </TableCell>
+                                                <TableCell>
+                                                    <TextField
+                                                        value={companyData.address}
+                                                        onChange={(e) => handleInputChange('address', e.target.value)}
+                                                    />
+                                                </TableCell>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <TableCell>{companyData.name}</TableCell>
+                                                <TableCell>{companyData.address}</TableCell>
+                                            </>
+                                        )}
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </div>
+
                 )}
             </Stack>
         </div>
     );
 }
 
-export default CompanyComponent;
+export default CompanyComponent;    
