efc8d7dfa12d0c5ab4ecd1ee9133771ab686a73f
Strahinja Banjanac <strahinja.banjanac@gmail.com>
2023-11-19 21:11:09 +0100

    feat: Equipments

--- isa2023front/src/App.js
+++ isa2023front/src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import UserViewContainer from './containers/UserViewContainer/UserViewContainer';
 import CompanyComponent from './components/companyComponent/companyComponent';
 
+
 import { ThemeProvider } from '@mui/material';
 import theme from './styles/theme';
 
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import AllCompaniesComponent from './components/allCompaniesComponent/allCompaniesComponent';
+import EquipmentSearchComponent from './components/equipmentSearchComponent/equipmentSearchComponent';
 import HomePageContainer from './containers/homePageContainer/home-page-container';
 import RegisterUserContainer from './containers/registerUserContainer/register-user-container';
+import EquipmentsSearchContainer from "./containers/equipmentsSearchContainer/EquipmentsSearchContainer";
+import EquipmentComponent from "./components/equipmentComponent/equipmentComponent";
 
 //treba zamijeniti theme.js u stilovima kada se bude stilizovala aplikacija
 function App() {
@@ -21,8 +25,10 @@ function App() {
           <Route path="/user/page/:id" element={<UserViewContainer/>}></Route>
           <Route path="/companies" element={<AllCompaniesComponent/>}></Route>
           <Route path="/company/:id" element={<CompanyComponent/>}></Route>
+          <Route path="/equipment/:id" element={<EquipmentComponent/>}></Route>
           <Route path="/home" element={<HomePageContainer/>}></Route>
           <Route path="/register" element={<RegisterUserContainer/>}></Route>
+          <Route path="/equipments" element={<EquipmentsSearchContainer/>}></Route>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
